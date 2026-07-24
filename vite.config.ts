284d4541037e34470c7cb6c4ba23ca8b307dd3ae
Spiffier <cgautreau343@gmail.com>
2026-07-23 23:15:29 -0400

    Update Site Creator database ID

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "4fb519d8-58d7-41de-ba5c-52e60383cf1b";
 
 const { d1, r2 } = hostingConfig;
 
